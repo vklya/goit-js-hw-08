5570c8c474116dd46223117751c021a166a71b36
Viktoria Prysiazhniuk <glushenkovika@gmail.com>
2022-12-01 12:52:11 +0200

    add new code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const ulGallery = document.querySelector('.gallery');
+
+const  galleryItem = ({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+    />
+    </a>
+    </li>`
+}
+
+const galleryCards = galleryItems.map(galleryItem).join('');
+ulGallery.insertAdjacentHTML('afterbegin', galleryCards);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const VIDEO_TIME_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({time}) {
+    localStorage.setItem(VIDEO_TIME_KEY, time);
+}
+
+setCurrentTime();
+
+function setCurrentTime () {
+    if (!localStorage.getItem(VIDEO_TIME_KEY)) {
+        return;
+    }
+    player.setCurrentTime(localStorage.getItem(VIDEO_TIME_KEY));
+}
 
